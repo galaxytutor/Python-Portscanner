bc6d7f16242880883b1d4159658b9fb32dc62a5c
L1GHTNER <109334667+L1GHTNER@users.noreply.github.com>
2022-07-15 22:32:30 -0400

    1.1 update

--- portscanner.py
+++ portscanner.py
@@ -11,19 +11,25 @@ else:
 	print("Invalid amount of arguments.")
 	print("Syntax: python3 portscanner.py <ip>")
 
-#add a pretty banner
+#add a banner
+print("-" * 50)
+print("█░░ ▄█ █▀▀ █░█ ▀█▀ █▄░█ █▀▀ █▀█ ▀ █▀   █▀█ █▀█ █▀█ ▀█▀   █▀ █▀▀ ▄▀█ █▄░█ █▄░█ █▀▀ █▀█")
+print("█▄▄ ░█ █▄█ █▀█ ░█░ █░▀█ ██▄ █▀▄ ░ ▄█   █▀▀ █▄█ █▀▄ ░█░   ▄█ █▄▄ █▀█ █░▀█ █░▀█ ██▄ █▀▄")
+print("Coded by L1GHTNER in Python")
+print("Version 1.1 OMEGA")
+print("-" * 50)
+
+#add another banner
+print('\n')
 print("-" * 50)
 print("Scanning target "+target)
 print("Time started: "+str(datetime.now()))
-print("This scanner is garbo btw")
-print("Coded by L1GHTNER in Python")
-print("Version 1.0 PORTSCANNER")
 print("-" * 50)
 
 try: 
-	for port in range(1,150):
+	for port in range(1,1000):
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		socket.setdefaulttimeout(.001)
+		socket.setdefaulttimeout(.0001)
 		result = s.connect_ex((target,port)) #returns an error indicator
 		#print("Checking port {}".format(port)) #commented out for a clean interface
 		if result == 0:
